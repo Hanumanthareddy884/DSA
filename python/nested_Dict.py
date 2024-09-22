c3bc16bb8d9a43f6282751b8efeb456432053d55
Hanumanthareddy <reddyreddy884@gmail.com>
2024-09-22 15:12:23 +0530

    Personal interview question I am faced in 2024 feb

--- python/nested_Dict.py
+++ python/nested_Dict.py
@@ -1,20 +1,19 @@
 myfamily = {
-  "child1" : {
-    "name" : "Emil",
-    "year" : 2004
-  },
-  "child2" : {
-    "name" : "Tobias",
-    "year" : 2007
-  },
-  "child3" : {
-    "name" : "Linus",
-    "year" : 2011
-  }
+    "child1": {
+        "name": "Emil",
+        "year": 2004
+    },
+    "child2": {
+        "name": "Tobias",
+        "year": 2007
+    },
+    "child3": {
+        "name": "Linus",
+        "year": 2011
+    }
 }
 
-d = list(map(lambda a:myfamily[a]['name'],myfamily))
+d = list(map(lambda a: myfamily[a]['name'], myfamily))
 print(d)
 # a = list(map(lambda a:myfamily[a].keys(),myfamily))
 # print(a)
-
