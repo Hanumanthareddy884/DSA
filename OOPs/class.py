00b5118837a32b82e6bbb6a7a606437c281f642b
Hanumanthareddy <reddyreddy884@gmail.com>
2024-09-29 15:12:27 +0530

    Class oops

---
+++ OOPs/class.py
@@ -0,0 +1,22 @@
+# Class declare
+
+class Student:
+    count=0
+    def __del__(self):
+        print("rdd","Destructor") # This is destructor is excute lastly and automatically call
+
+    def registration(self):
+        print("reddy","Reg method")
+
+
+    def __init__(self,name,age) :
+        self.name = name
+        self.age = age
+        print("redd","Constructor") #This is excute first in becuse its constructor and automatically call
+
+    def __str__(self):
+        self.registration()
+        return f"{self.name}({self.age})"
+
+std = Student("Atharva",2)
+print(std)
